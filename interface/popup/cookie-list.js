35635d1d2c6af2257fba0b5aa84bcd0fbd885b4a
tienanhemho <tienanhemho@gmail.com>
2022-11-25 22:44:36 +0700

    T
    T

--- interface/popup/cookie-list.js
+++ interface/popup/cookie-list.js
@@ -293,18 +293,18 @@
             try {
                 cookies = JSON.parse(json);
             } catch (error) {
-                console.log("Couldn't parse Json", error);
-                sendNotification("Could not parse the Json value");
-                buttonIcon.setAttribute("href", "../sprites/solid.svg#times");
-                setTimeout(() => {
-                    buttonIcon.setAttribute("href", "../sprites/solid.svg#file-export");
-                }, 1500);
-                return;
+                // console.log("Couldn't parse Json", error);
+                // sendNotification("Could not parse the Json value");
+                // buttonIcon.setAttribute("href", "../sprites/solid.svg#times");
+                // setTimeout(() => {
+                //     buttonIcon.setAttribute("href", "../sprites/solid.svg#file-export");
+                // }, 1500);
+                // return;
+                cookies = extractCookies(json);
             }
-
             if (!isArray(cookies)) {
-                console.log("Invalid Json");
-                sendNotification("The Json is not valid");
+                console.log("Check cookies format");
+                sendNotification("Nothing to import or couldn't parse Json/Netscape");
                 buttonIcon.setAttribute("href", "../sprites/solid.svg#times");
                 setTimeout(() => {
                     buttonIcon.setAttribute("href", "../sprites/solid.svg#file-export");
@@ -676,5 +676,48 @@
                 cookieElement.classList.add('hide');
             }
         };
+    }
+    function extractCookies(text) {
+
+        var cookies = [];
+        var lines = text.split("\n");
+     
+        // iterate over lines
+        lines.forEach(function(line, index){
+            
+            var tokens = line.split("\t");
+     
+            // we only care for valid cookie def lines
+            if (tokens.length == 7) {
+     
+                // trim the tokens
+                tokens = tokens.map(function(e){return e.trim();});
+     
+                var cookie = {};
+     
+                // Extract the data
+                cookie.domain = tokens[0];
+                cookie.flag = tokens[1] === 'TRUE';
+                cookie.path = tokens[2];
+                cookie.secure = tokens[3] === 'TRUE';
+     
+                // Convert date to a readable format
+                
+                var timestamp = tokens[4];
+                if (timestamp.length == 17){
+                    timestamp = Math.floor(timestamp / 1000000 - 11644473600);
+                }
+                
+                cookie.expiration = timestamp;
+     
+                cookie.name = tokens[5];
+                cookie.value = tokens[6];
+     
+                // Record the cookie.
+                cookies.push(cookie);
+            }    
+        });
+        
+        return cookies;
     }
 }());
